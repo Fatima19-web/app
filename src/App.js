16821b03705b5e740d627a6199049e24a4122dae
fatima <faizamanzoor947@gmail.com>
2025-12-02 23:36:31 +0500

    Upload new food-app project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 
-import './App.css';
+import React from "react";
 
-
-function App() {
-
-  const showMessage = () => {
-    alert("Hello from React!");
-  };
-
+function Intro() {
   return (
-    <div className="box">
-      <h1>Welcome to My React App 🎉</h1>
-      <button className="btn" onClick={showMessage}>Click Me</button>
+    <div> 
+      <h1><b>Introduction</b></h1>
+      <p>My name is Fatima.</p>
+      <p>I am in class 10.</p>
+      <p>
+        I love sketching, writing, and travelling. I enjoy exploring new ideas,
+        learning creative skills, and expressing myself through art. React is
+        new for me, but I’m excited to learn and build more projects!
+      </p>
     </div>
   );
 }
 
-export default App;
+export default Intro;
+
+
+
+
